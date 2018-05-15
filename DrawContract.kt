ca6fe10c377edc8be796b6f4e21090803c61d662
Tasos Kichidis <a.kichidis@elsevier.com>
2018-05-15 17:05:16 -0400

    updated contract

--- DrawContract.kt
+++ DrawContract.kt
@@ -24,7 +24,7 @@ open class DrawContract : Contract {
 
                 val drawState = tx.outputsOfType<DrawState>().first()
 
-                "Duplicate ticket ids found " using (getDedupedTicketIds(drawState).size == drawState.drawParticipants.size)
+                "Duplicate ticket ids found " using containsUniqueTickets(drawState)
             }
         }
 
@@ -35,11 +35,11 @@ open class DrawContract : Contract {
 
             //require to have only 1 command of that type
             val command = tx.commands.requireSingleCommand<DrawState.StockIndexPricesCommand>()
-            val oraclePublicKey = drawState.state.data.oracle.owningKey
+            val oraclePublicKey = stateData.oracle.owningKey
 
             "Provided stock index data is wrong" using (command.value.index == stateData.index)
 
-            "Command signer is other that state's oracle" using (command.signers.contains(oraclePublicKey))
+            "Command signer is other than state's oracle" using (command.signers.contains(oraclePublicKey))
 
             val seed = command.value.prices
 
@@ -47,11 +47,11 @@ open class DrawContract : Contract {
             val participantResults = stateData.drawParticipants.map { p -> Pair(p, toInt(SecureHash.sha256(p.ticketId + seed))) }
 
             //sort  based on the hash int results
-            sort(participantResults, { p1, p2 -> -p1.second.compareTo(p2.second) })
+            sort(participantResults, { p1, p2 -> p1.second.compareTo(p2.second) })
 
-            val winner = participantResults.get(0)
+            val winner = participantResults.last()
 
-            "Party other than the winner tries to spend the state" using command.signers.contains(winner.first.party.owningKey)
+            "Only the winner can spend the state" using command.signers.contains(winner.first.party.owningKey)
         }
     }
 
@@ -59,7 +59,7 @@ open class DrawContract : Contract {
         return BigInteger(hash.toString(), 16).abs()
     }
 
-    private fun getDedupedTicketIds(drawState: DrawState): Set<String> {
-        return drawState.drawParticipants.map { p -> p.ticketId }.toSet()
+    private fun containsUniqueTickets(drawState: DrawState): Boolean {
+        return drawState.drawParticipants.map { p -> p.ticketId }.toSet().size == drawState.drawParticipants.size
     }
 }
